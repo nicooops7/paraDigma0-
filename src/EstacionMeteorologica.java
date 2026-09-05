7daac8ba0f7a81d9e81db733a826df4c05fa07c7
estekal <estekal.orig@gmail.com>
2026-09-05 13:45:16 -0400

    Creación de rama personal: feature/esteban

---
+++ src/EstacionMeteorologica.java
@@ -0,0 +1,2 @@
+public class EstacionMeteorologica {
+}
---
+++ src/Region.java
@@ -0,0 +1,29 @@
+public class Region {
+    private int codigo;
+    private String nombre;
+
+    public Region(int cod, String nom){
+        this.codigo=cod;
+        this.nombre=nom;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public boolean addComuna(String nom, int cod){
+        if(==cod||.equals(nom)){
+
+        }
+    }
+    public Comuna findComunaByld(int codigo){
+
+    }
+    public Comuna[] getComunas(){
+
+    }
+    public int getCantidadEstaciones(){
+
+    }
+}
