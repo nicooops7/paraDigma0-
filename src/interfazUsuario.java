0d21702580619ffd8a5af9452bd9d7345665c10c
jojoab <jojoab586@gmail.com>
2026-09-10 10:16:49 -0300

    avance main y institutoMetereologia

--- src/interfazUsuario.java
+++ src/interfazUsuario.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
 public class interfazUsuario {
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -10,4 +11,42 @@ public class interfazUsuario {
 
 
     }
+    private void menuPrincipal(){
+
+    }
+    private void crearRegion(){
+
+    }
+    private void crearComuna(){
+
+    }
+    private void crearEstacionMeteorologica(){
+
+    }
+    private void instalarSensor(){
+
+    }
+    private void registrarMedicion(){
+
+    }
+    private void menuListados(){
+
+    }
+    public void listarRegiones(){
+
+    }
+    public void listarComunas(){
+
+    }
+    public void listarEstaciones(){
+
+    }
+    public void listarSensores(){
+
+    }
+    public void listarMediciones(){
+
+    }
+
+
 }
