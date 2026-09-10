8a749dca34e093de0b6707518f6939f9c8c42ffc
jojoab <jojoab586@gmail.com>
2026-09-10 10:04:00 -0300

    avance main y institutoMetereologia

--- src/institutoMetereologia.java
+++ src/institutoMetereologia.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 public class institutoMetereologia {
     private ArrayList<Region> regiones;
@@ -18,14 +19,35 @@ public class institutoMetereologia {
     }
     public boolean creaComuna(int codigo, String nombre , int codigoRegion){
         for(Region region : regiones){
-            for(region.getCodigo() == codigoRegion){
-                return region.addComuna(codigo, nombre);
+            if(region.getCodigo() == codigoRegion){
+                return region.addComuna(nombre, codigo);
             }
         }
         return false;
     }
-    public boolean creaEstacion(String cod, String nombre, float ion , float lat, float alt, int codRegion, int codComuna){
+    public boolean creaEstacion(String cod, String nombre, float lon , float lat, float alt, int codRegion, int codComuna){
 
     }
 
+    public boolean instalaSensor(String cod,String marca, String modelo, TipoSensor tipo, String codigoEstacion){
+
+    }
+    public boolean registraMedicion(LocalDateTime fechaHora,float valor, String codEstacion, String codSensor){
+
+    }
+    public String[][] listaRegiones(){
+
+    }
+    public String[][] listaComunas(){
+
+    }
+    public String[][] listaEstaciones(int codigoRegion, int codigoComuna){
+
+    }
+    public String[][] listaSensores(String codigoEstacion){
+
+    }
+    public String[][] listaMediciones(String codEstacion, String codSensor, LocalDateTime inicio, LocalDateTime fin){
+
+    }
 }
---
+++ src/interfazUsuario.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+public class interfazUsuario {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+    }
+}
